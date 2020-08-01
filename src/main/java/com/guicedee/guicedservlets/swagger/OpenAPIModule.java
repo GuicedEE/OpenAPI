09b05adaa209bfbc46aa5196beacaf09eb2f2422
GedMarc <ged_marc@hotmail.com>
2020-08-01 02:04:12 +0200

    Fix OpenAPI

--- src/main/java/com/guicedee/guicedservlets/swagger/OpenAPIModule.java
+++ src/main/java/com/guicedee/guicedservlets/swagger/OpenAPIModule.java
@@ -1,12 +1,8 @@
 package com.guicedee.guicedservlets.swagger;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.guicedee.guicedservlets.swagger.implementations.SwaggerServlet;
-import com.guicedee.guicedservlets.swagger.jsonoverrides.Json;
 import com.guicedee.guicedservlets.services.GuiceSiteInjectorModule;
 import com.guicedee.guicedservlets.services.IGuiceSiteBinder;
+import com.guicedee.guicedservlets.swagger.implementations.SwaggerServlet;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -39,13 +35,6 @@ public class OpenAPIModule
 	@Override
 	public void onBind(GuiceSiteInjectorModule module)
 	{
-		Json.mapper()
-		    .enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING)
-		    .enable(JsonGenerator.Feature.QUOTE_FIELD_NAMES)
-		    .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
-		    .setSerializationInclusion(JsonInclude.Include.NON_NULL)
-		    .disable(SerializationFeature.FAIL_ON_UNWRAPPED_TYPE_IDENTIFIERS);
-
 		Map<String, String> props = new HashMap<>();
 		props.put("jersey.config.server.wadl.disableWadl", "true");
 		props.put("jersey.config.server.provider.packages", "");
