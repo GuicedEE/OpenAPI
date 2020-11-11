b88067ef8b2455a373d099149c8becee696dad30
GedMarc <ged_marc@hotmail.com>
2020-11-12 01:34:06 +0200

    Jakarta Namespace - JLink building

---
+++ src/jre11/java/module-info.java
@@ -0,0 +1,18 @@
+module com.guicedee.guicedservlets.openapi {
+	requires transitive com.guicedee.services.openapi;
+	requires transitive com.guicedee.guicedservlets.rest;
+
+	requires java.xml.bind;
+	requires java.servlet;
+
+	exports com.guicedee.guicedservlets.swagger;
+	exports com.guicedee.guicedservlets.swagger.services;
+
+	uses com.guicedee.guicedservlets.swagger.services.IGuicedSwaggerConfiguration;
+
+	opens com.guicedee.guicedservlets.swagger to com.google.guice;
+	opens com.guicedee.guicedservlets.swagger.implementations to com.google.guice;
+
+	provides com.guicedee.guicedservlets.services.IGuiceSiteBinder with com.guicedee.guicedservlets.swagger.OpenAPIModule;
+
+}
